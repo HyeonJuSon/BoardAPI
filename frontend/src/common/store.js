4a944c9e3516deccbea551cf4e3e97bb728b3608
HyeonJu Son <kkobukk95@gmail.com>
2021-10-16 22:10:34 +0900

    [UPDATE] frontend settings ..ing

---
+++ frontend/src/common/store.js
@@ -0,0 +1,8 @@
+import { createStore } from "vuex";
+import root from "@/store";
+
+export default createStore({
+    modules: {
+        root,
+    },
+});
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,15 @@
-import { createApp } from 'vue'
+import { createApp, h } from 'vue'
 import App from './App.vue'
+// router
+import router from './router'
+// axios
+import VueAxios from './common/axios'
+import axios from './common/axios'
 
+const app = createApp({
+    render: () => h(App)
+});
+app.use(VueAxios, axios)
+app.use(store)
+app.use(router)
 createApp(App).mount('#app')
