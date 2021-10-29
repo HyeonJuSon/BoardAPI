f182fb3738fc8b9e6d87b517df59c61d2be8dcb2
HyeonJu Son <kkobukk95@gmail.com>
2021-10-29 22:31:53 +0900

    [UPDATE] actions.js

--- frontend/src/store/actions.js
+++ frontend/src/store/actions.js
@@ -2,9 +2,9 @@
 import $axios from 'axios';
 
 export function join({ state }, User) {
-    return $axios.post("/son/user/join", User);
+    return $axios.post("/son/api/user/join", User);
 }
 
 export function getUser({ state }, userid) {
-    return $axios.get(`/son/user/${userid}`);
+    return $axios.get(`/son/api/user/${userid}`);
 }
